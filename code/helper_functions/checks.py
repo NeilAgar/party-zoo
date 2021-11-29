4bc0a0f1901634c428eccfe33ee9deff17673385
NeilAgar <aditneil@outlook.com>
2021-11-28 18:00:19 -0600

    IEP Final Update

--- code/helper_functions/checks.py
+++ code/helper_functions/checks.py
@@ -2,7 +2,7 @@ from code.modules.wiimotes import wiimote_initializer
 
 
 def wiimote_number_check(wiimote_count):
-    if not (wiimote_count > 4 or wiimote_count < 2):
+    if 0 <= wiimote_count <= 4:
         return wiimote_initializer()
 
     print("Wiimote count not valid (2 - 4 players only)")
